0bcaeb546886f167ab231626555202c32b451635
snowflaki <vichepolskiyura@gmail.com>
2023-06-20 20:01:43 +0300

    refactor PasswordComponent

--- password-test/src/app/components/password/password.component.ts
+++ password-test/src/app/components/password/password.component.ts
@@ -1,32 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  FormBuilder,
+  FormGroup,
+} from '@angular/forms';
+import { PasswordStrengthService } from '../../services/calculate-password-strength.service';
 
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PasswordComponent),
+      multi: true,
+    },
+  ],
 })
-export class PasswordComponent {
-  password: string = '';
+export class PasswordComponent implements ControlValueAccessor {
+  passwordForm: FormGroup;
   strength: string = 'empty';
 
-  passwordHandler = () => {
-    if (this.password.length === 0) {
-      this.strength = 'empty';
-    } else if (!this.password || this.password.length < 8) {
-      this.strength = 'small';
-    } else if (
-      /[a-zA-Z]/.test(this.password) &&
-      /\d/.test(this.password) &&
-      /\W/.test(this.password)
-    ) {
-      this.strength = 'strong';
-    } else if (
-      (/[a-zA-Z]/.test(this.password) && /\d/.test(this.password)) ||
-      (/[a-zA-Z]/.test(this.password) && /\W/.test(this.password)) ||
-      (/\d/.test(this.password) && /\W/.test(this.password))
-    ) {
-      this.strength = 'medium';
-    } else {
-      this.strength = 'easy';
-    }
-  };
+  strengthSections: { name: string }[] = [
+    { name: 'easy' },
+    { name: 'medium' },
+    { name: 'strong' },
+  ];
+
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private passwordStrengthService: PasswordStrengthService
+  ) {
+    this.passwordForm = this.formBuilder.group({
+      password: '',
+    });
+
+    this.passwordForm.valueChanges.subscribe(() => {
+      this.calculatePasswordStrength();
+      this.onChange(this.passwordForm.get('password')?.value);
+    });
+  }
+
+  writeValue(value: string): void {
+    this.passwordForm.get('password')?.setValue(value);
+    this.calculatePasswordStrength();
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  private calculatePasswordStrength() {
+    const password = this.passwordForm.get('password')?.value;
+    this.strength = this.passwordStrengthService.getPasswordStrength(password);
+  }
 }
