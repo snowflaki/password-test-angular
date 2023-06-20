6bdee6405a6c7f19056e3d9b267e996250d07dbe
snowflaki <vichepolskiyura@gmail.com>
2023-06-20 19:59:02 +0300

    add ReactiveFormsModule, Components and Providers to app.module.ts

--- password-test/src/app/app.module.ts
+++ password-test/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PasswordComponent } from './components/password/password.component';
+import { PasswordStrengthService } from './services/calculate-password-strength.service';
+import { PasswordStrengthSectionComponent } from './components/password-strength-section/password-strength-section.component';
 
 @NgModule({
-  declarations: [AppComponent, PasswordComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    PasswordComponent,
+    PasswordStrengthSectionComponent,
+  ],
+  imports: [BrowserModule, ReactiveFormsModule],
+  providers: [PasswordStrengthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
